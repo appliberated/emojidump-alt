d93de9a39569dea9ef8f24333ee0fabf496d20ed
helloworldwriter <writer@helloworldwriter.com>
2018-04-10 21:35:47 +0300

    Add help screen; improve dump command workflow

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,8 @@
+/*
+ *  emojidump. Copyright (c) 2018 HWALab. MIT License.
+ *  https://www.hwalab.com/emojidump/
+ */
+
 /* eslint-disable no-console */
 
 import * as utils from "/scripts/utils.js";
@@ -5,41 +10,45 @@ import * as utils from "/scripts/utils.js";
 // [...new Set(emojiList.map(item => item.v))].sort((a, b) => a - b).join(", ")
 const UNICODE_VERSIONS = new Set([1.1, 3.0, 3.2, 4.0, 4.1, 5.1, 5.2, 6.0, 6.1, 7.0, 8.0, 9.0, 10.0, 11.0]);
 
-let emojiList;
+const DEFAULT_ZOM = 2;
+
+let sourceEmojiList;
 let curEmojiList;
 const emojiDumpEl = document.getElementById("emojiDump");
 const commandEl = document.getElementById("command");
+const feedbackEl = document.getElementById("feedback");
+const helpEl = document.getElementById("help");
 
 
 fetch("/scripts/emoji.json")
     .then((response) => response.json())
     .then((data) => {
-        emojiList = data;
-        dump();
+        sourceEmojiList = data;
+        dumpCommand();
     });
 
 
-function dump() {
+function updateDump() {
     const args = utils.basicCLAParser(commandEl.value);
     console.log(args);
-
-    // By default display all emojis
-    curEmojiList = emojiList;
 
     // Parse the Unicode version argument
     if (args.hasOwnProperty("u")) {
         const version = args["u"];
-        if (!UNICODE_VERSIONS.has(version)) return `Invalid Unicode version: ${version}`;
+        if (!UNICODE_VERSIONS.has(version)) return { result: false, msg: `Invalid Unicode version: ${version}` };
 
         // Filter emojis by Unicode version
         console.log(`Filtering Unicode v${version} emojis...`);
-        curEmojiList = emojiList.filter(el => el.v <= version);
+        curEmojiList = sourceEmojiList.filter(el => el.v <= version);
+    } else {
+        // By default display all emojis
+        curEmojiList = sourceEmojiList.slice(0);
     }
 
     // Parse the shuffle argument
     if (args.hasOwnProperty("s")) {
         const shuffle = args["s"];
-        if (typeof shuffle != typeof true) return `Invalid shuffle option: ${shuffle}`;
+        if (typeof shuffle != typeof true) return { result: false, msg: `Invalid shuffle option: ${shuffle}` };
 
         // Shuffle emoji array
         if (shuffle) {
@@ -51,7 +60,7 @@ function dump() {
     // Parse the limit argument
     if (args.hasOwnProperty("l")) {
         const limit = args["l"];
-        if (!Number.isInteger(limit)) return `Invalid limit value: ${limit}`;
+        if (!Number.isInteger(limit)) return { result: false, msg: `Invalid limit value: ${limit}` };
 
         // Slice emoji array
         console.log(`Slicing the first ${limit} emojis...`);
@@ -59,18 +68,18 @@ function dump() {
     }
 
     // Parse the zoom argument
+    let zoom = DEFAULT_ZOM;
     if (args.hasOwnProperty("z")) {
-        const zoom = args["z"];
-        if (!utils.isIntegerBetween(zoom, 1, 4)) return `Invalid zoom value: ${zoom}`;
-
-        emojiDumpEl.dataset.zoom = zoom;
+        zoom = args["z"];
+        if (!utils.isIntegerBetween(zoom, 1, 4)) return { result: false, msg: `Invalid zoom value: ${zoom}` };
     }
+    emojiDumpEl.dataset.zoom = zoom;
 
     // Parse the join argument
     let separator = " ";
     if (args.hasOwnProperty("j")) {
         const join = args["j"];
-        if (typeof join != typeof true) return `Invalid join option: ${join}`;
+        if (typeof join != typeof true) return { result: false, msg: `Invalid join option: ${join}` };
 
         if (join) separator = "";
     }
@@ -81,16 +90,21 @@ function dump() {
 
     // Update emoji count
     document.title = `emojidump - ${curEmojiList.length} emojis`;
+
+    return { result: true, msg: `` };
+}
+
+function dumpCommand() {
+    const {result, msg} = updateDump();
+    console.log(result + msg);
+    emojiDumpEl.dataset.visible = result;
+    helpEl.dataset.visible = !result;
+    feedbackEl.innerText = msg;
 }
 
 commandEl.addEventListener("keyup", (event) => {
     if (event.which === 13) {
         commandEl.blur();
-
-        const errorMsg = dump();
-        if (errorMsg) {
-            emojiDumpEl.innerText = errorMsg;
-            console.log(errorMsg);
-        }
+        dumpCommand();
     }
 });
--- docs/scripts/utils.js
+++ docs/scripts/utils.js
@@ -1,3 +1,8 @@
+/*
+ *  emojidump. Copyright (c) 2018 HWALab. MIT License.
+ *  https://www.hwalab.com/emojidump/
+ */
+
 export function isBetween(n, a, b) {
     return (n - a) * (n - b) <= 0
 }
@@ -6,6 +11,13 @@ export function isIntegerBetween(n, a, b) {
     return Number.isInteger(n) && (n - a) * (n - b) <= 0
 }
 
+/**
+ * Randomize array element order in-place, using an implementation of the Durstenfeld shuffle, a
+ * computer-optimized version of Fisher-Yates shuffle.
+ * Credit: {@link https://stackoverflow.com/a/12646864/220039}
+ * @param {Array} array The array to shuffle.
+ * @returns {void}
+ */
 export function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -18,7 +30,7 @@ export function basicCLAParser(claString) {
 
         // let [k, v = true] = arg.split('=')
         // acc[k] = v
-        let [k, v] = arg.split('=');
+        let [k, v] = arg.split(':');
         acc[k] = v === undefined ? true : /^(true|false)$/i.test(v) ? v.toLowerCase() === "true" : /^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/.test(v) ? Number(v) : v;
         return acc
     
