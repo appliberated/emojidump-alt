df0327095f804a62687545583cba6f4a5d91a7ad
helloworldwriter <writer@helloworldwriter.com>
2018-04-11 10:53:51 +0300

    Add command status message; improve UI

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -8,9 +8,11 @@
 import * as utils from "/scripts/utils.js";
 
 // [...new Set(emojiList.map(item => item.v))].sort((a, b) => a - b).join(", ")
-const UNICODE_VERSIONS = new Set([1.1, 3.0, 3.2, 4.0, 4.1, 5.1, 5.2, 6.0, 6.1, 7.0, 8.0, 9.0, 10.0, 11.0]);
+const UNICODE_VERSIONS = [1.1, 3.0, 3.2, 4.0, 4.1, 5.1, 5.2, 6.0, 6.1, 7.0, 8.0, 9.0, 10.0, 11.0];
 
 const DEFAULT_ZOM = 2;
+
+const CLI_OPTIONS = { UNICODE_VERSION: "-u", SHUFFLE: "-s", MAX: "-m", ZOOM: "-z", JOIN: "-j" }
 
 let sourceEmojiList;
 let curEmojiList;
@@ -29,56 +31,53 @@ fetch("/scripts/emoji.json")
 
 
 function updateDump() {
+    let msg = "";
+
     const args = utils.basicCLAParser(commandEl.value);
     console.log(args);
 
     // Parse the Unicode version argument
-    if (args.hasOwnProperty("u")) {
-        const version = args["u"];
-        if (!UNICODE_VERSIONS.has(version)) return { result: false, msg: `Invalid Unicode version: ${version}` };
+    let version = UNICODE_VERSIONS[UNICODE_VERSIONS.length - 1];
+    if (args.hasOwnProperty(CLI_OPTIONS.UNICODE_VERSION)) {
+        version = args[CLI_OPTIONS.UNICODE_VERSION];
+        if (!UNICODE_VERSIONS.includes(version)) return { result: false, msg: `Invalid Unicode version: ${version}` };
 
         // Filter emojis by Unicode version
-        console.log(`Filtering Unicode v${version} emojis...`);
+        console.log(`Filtering Unicode ${version} emojis...`);
         curEmojiList = sourceEmojiList.filter(el => el.v <= version);
     } else {
         // By default display all emojis
         curEmojiList = sourceEmojiList.slice(0);
     }
+    msg = `${curEmojiList.length.toLocaleString()} Unicode ${version.toFixed(1)} emojis`;
 
     // Parse the shuffle argument
-    if (args.hasOwnProperty("s")) {
-        const shuffle = args["s"];
+    if (args.hasOwnProperty(CLI_OPTIONS.SHUFFLE)) {
+        const shuffle = args[CLI_OPTIONS.SHUFFLE];
         if (typeof shuffle != typeof true) return { result: false, msg: `Invalid shuffle option: ${shuffle}` };
 
         // Shuffle emoji array
         if (shuffle) {
-            console.log(`Shuffling emojis...`);
             utils.shuffleArray(curEmojiList);
+            msg += ", shuffled";
         }
     }
 
     // Parse the limit argument
-    if (args.hasOwnProperty("l")) {
-        const limit = args["l"];
-        if (!Number.isInteger(limit)) return { result: false, msg: `Invalid limit value: ${limit}` };
+    if (args.hasOwnProperty(CLI_OPTIONS.MAX)) {
+        const max = args[CLI_OPTIONS.MAX];
+        if (!Number.isInteger(max)) return { result: false, msg: `Invalid maximum number of emojis to display: ${max}` };
 
         // Slice emoji array
-        console.log(`Slicing the first ${limit} emojis...`);
-        curEmojiList = curEmojiList.slice(0, limit);
+        curEmojiList = curEmojiList.slice(0, max);
+        msg = `${max.toLocaleString()} of ${msg}`;
     }
-
-    // Parse the zoom argument
-    let zoom = DEFAULT_ZOM;
-    if (args.hasOwnProperty("z")) {
-        zoom = args["z"];
-        if (!utils.isIntegerBetween(zoom, 1, 4)) return { result: false, msg: `Invalid zoom value: ${zoom}` };
-    }
-    emojiDumpEl.dataset.zoom = zoom;
+    msg = `Dumped ${msg}`;
 
     // Parse the join argument
     let separator = " ";
-    if (args.hasOwnProperty("j")) {
-        const join = args["j"];
+    if (args.hasOwnProperty(CLI_OPTIONS.JOIN)) {
+        const join = args[CLI_OPTIONS.JOIN];
         if (typeof join != typeof true) return { result: false, msg: `Invalid join option: ${join}` };
 
         if (join) separator = "";
@@ -87,15 +86,25 @@ function updateDump() {
     // Do emoji dump
     console.log(`Dumping ${curEmojiList.length} emojis with ${separator === "" ? "no" : "space"} separator...`);
     emojiDumpEl.innerText = curEmojiList.map(emoji => emoji.e).join(separator);
+    msg = `${msg}, with ${separator === "" ? "no" : "space"} separator`;
+
+    // Parse the zoom argument
+    let zoom = DEFAULT_ZOM;
+    if (args.hasOwnProperty(CLI_OPTIONS.ZOOM)) {
+        zoom = args[CLI_OPTIONS.ZOOM];
+        if (!utils.isIntegerBetween(zoom, 1, 4)) return { result: false, msg: `Invalid zoom value: ${zoom}` };
+    }
+    emojiDumpEl.dataset.zoom = zoom;
+    msg += `, and a ${zoom}x zoom`;
 
     // Update emoji count
     document.title = `emojidump - ${curEmojiList.length} emojis`;
 
-    return { result: true, msg: `` };
+    return { result: true, msg: msg };
 }
 
 function dumpCommand() {
-    const {result, msg} = updateDump();
+    const { result, msg } = updateDump();
     console.log(result + msg);
     emojiDumpEl.dataset.visible = result;
     helpEl.dataset.visible = !result;
